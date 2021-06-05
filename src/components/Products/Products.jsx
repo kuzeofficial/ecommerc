6065da1461259d81730acb09310d7855886198be
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-05 10:19:53 -0700

    Final Steps change

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { Grid } from '@material-ui/core';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
-import Product from '../Product/Product';
-import useStyles from './styles'
+import Product from './Product/Product';
+import useStyles from './styles';
 
-const Products = ({ products }) => {
-    const classes = useStyles();
-    return(
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
-                    </Grid>
-                )) }
-            </Grid>
-        </main>
-    )
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
 
-}
+  if (!products.length) return <p>Loading...</p>;
 
-export default Products
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
+
